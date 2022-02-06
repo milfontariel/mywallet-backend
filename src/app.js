56cf25125fa25879710edb76b42fd6c4b3b7f476
ariel <ariel1242009@gmail.com>
2022-02-06 20:19:29 -0300

    add heroku port

--- src/app.js
+++ src/app.js
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
-app.listen(5000, () => {
-    console.log('Rodando na porta 5000');
-})
+app.listen(process.env.PORT, () => {
+    console.log('Server running on port ' + process.env.PORT);
+});
